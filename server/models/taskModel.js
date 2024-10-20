68337735c8ead38e7b3beb5fc1ba846d3acefd08
ImKetan1610 <ketan.makode@gmail.com>
2024-10-21 01:27:45 +0530

    added Task Schema

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,7 +1,25 @@
 const mongoose = require("mongoose");
 
+const checklistSchema = mongoose.Schema({
+  description: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+});
+
 const taskSchema = mongoose.Schema(
-  {},
+  {
+    title: { type: String, required: true },
+    priority: { type: String, required: true },
+    dueDate: { type: Date, default: null },
+    status: {
+      type: String,
+      enum: ["backlog", "to-do", "in-progress", "done"],
+      default: "backlog",
+    },
+    category: { type: String, required: false },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    sharedWith: [{ type: String, required: false }],
+    checklist: [checklistSchema],
+  },
   {
     versionKey: false,
     timestamps: true,
