0ea74904d8ce1033bb3f42c734f39c4e69b37083
ImKetan1610 <ketan.makode@gmail.com>
2024-11-02 12:52:37 +0530

    added share task page

--- client/src/Components/ShareTask/ShareTask.jsx
+++ client/src/Components/ShareTask/ShareTask.jsx
@@ -8,6 +8,8 @@ const ShareTask = () => {
   const params = useParams();
   const [data, setData] = useState({});
 
+  const completedCount = data?.checklist?.filter((item) => item.checked).length;
+
   // Use effect for fetching task details by ID
   useEffect(() => {
     const fetchTaskData = async () => {
@@ -20,28 +22,69 @@ const ShareTask = () => {
       }
     };
 
-    fetchTaskData(); 
-  }, []); 
+    fetchTaskData();
+  }, []);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { month: "short", day: "numeric" };
+    const formattedDate = date.toLocaleDateString("en-US", options);
+    const day = date.getDate();
+
+    const ordinalSuffix =
+      day % 10 === 1 && day !== 11
+        ? "st"
+        : day % 10 === 2 && day !== 12
+        ? "nd"
+        : day % 10 === 3 && day !== 13
+        ? "rd"
+        : "th";
+
+    return `${formattedDate}${ordinalSuffix}`;
+  }
 
   console.log("Task Data State:", data);
+  const formattedDate = formatDate(data?.dueDate);
+  console.log(formattedDate);
 
   return (
     <div className={s.taskCard}>
-      {/* Header with Priority and Options */}
       <div className={s.taskHeader}>
-        <div>
-          <div className={`${s[data?.priority?.toLowerCase()]}`}>&nbsp;</div>
-          <span className={`${s.priority}`}>
-            {data?.priority?.toUpperCase() || "N/A"}
-          </span>
+        <div className={`${s[data?.priority?.toLowerCase().split(" ")[0]]}`}>
+          &nbsp;
         </div>
+        <span className={`${s.priority}`}>{data?.priority?.toUpperCase()}</span>
       </div>
 
-      <h3 className={s.taskTitle}>{data.title || "Untitled Task"}</h3>
+      <h3 className={s.taskTitle}>{data.title}</h3>
 
-      <div className={s.taskDetails}>
-        <p>{data.description || "No description provided."}</p>
+      <div className={s.checklistHeader}>
+        <span>
+          Checklist ({completedCount}/{data?.checklist?.length})
+        </span>
       </div>
+      <ul className={s.checklist}>
+        {data?.checklist?.map((item, index) => (
+          <li key={index} className={s.checklistItem}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+              className={s.checkbox}
+            />
+            <span>{item.label}</span>
+          </li>
+        ))}
+      </ul>
+      {data?.dueDate && (
+        <div
+          className={`s.dueDateBox ${
+            s[data?.priority?.toLowerCase().split(" ").join("_")]
+          }`}
+        >
+          <span><strong>Due Date: </strong></span>
+          <span>{formattedDate}</span>
+        </div>
+      )}
     </div>
   );
 };
