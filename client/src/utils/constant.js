7c8a5efef1759477b2aa45abc6f79f5f6a5287b5
ImKetan1610 <ketan.makode@gmail.com>
2024-11-02 23:41:24 +0530

    working code added frontend url

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,2 +1,2 @@
 export const BACKEND_URL = "https://projectmanagementapp-vksm.onrender.com"
-export const FRONTEND_URL = "http://localhost:5173"
+export const FRONTEND_URL = "https://project-management-app-drab-beta.vercel.app"
