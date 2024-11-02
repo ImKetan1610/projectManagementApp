365e6f3a2865feea03f825081b4aa07e6f2d9329
ImKetan1610 <ketan.makode@gmail.com>
2024-11-02 22:49:51 +0530

    working code push need to check the bug of addPeople route

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "https://projectmanagementapp-vksm.onrender.com"
-export const BACKEND_URL = "http://localhost:8082"
+export const BACKEND_URL = "https://projectmanagementapp-vksm.onrender.com"
+// export const BACKEND_URL = "http://localhost:8082"
 export const FRONTEND_URL = "http://localhost:5173"
